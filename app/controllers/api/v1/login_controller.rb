55300c2c38d1a317a0fedd46ee2a03a25e12aab7
沈斌 <bluelazysb@hotmail.com>
2018-04-07 21:48:09 +0800

    updates

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -7,14 +7,14 @@ class Api::V1::LoginController < Api::V1::BaseController
     username = params[:username]
     password = params[:password]
     admin = Admin.find_by(username: username)
-    if admin && admin.authenticate(password)
-      if admin.activated?
+    if admin
+      if admin.authenticate(password) && admin.activated?
         render :json => { status: 'ok', type: params[:type], currentAuthority: 'admin' }
       else
-        render :json => { status: 'error', type: params[:type], currentAuthority: 'guest' }
+        render :json => { status: 'error', type: params[:type], currentAuthority: 'guest', content: "账户或密码错误" }
       end
     else
-      render :json => { status: 'error', type: params[:type], currentAuthority: 'guest' }
+      render :json => { status: 'error', type: params[:type], currentAuthority: 'guest', content: "该账户不存在" }
     end
 
   end
