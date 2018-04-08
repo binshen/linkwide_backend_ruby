2736396af55a41552e125432124b9abdbece50e0
沈斌 <bluelazysb@hotmail.com>
2018-04-08 15:09:31 +0800

    updates

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -14,9 +14,38 @@ class Api::V1::CustomersController < Api::V1::BaseController
   end
 
   def create
-    puts "=======================2"
-    puts params
-    render json: { status: 1 }
+    method = params[:method]
+    if method == 'post'
+      if params[:name]
+        @customer = Customer.new(customer_params)
+        @customer.save
+        render json: { status: 1 }
+      else
+        render json: { status: 0 }
+      end
+    else
+      if params[:id]
+        customer_ids = params[:id].to_s.split(',')
+        if method == 'remove'
+          Customer.delete(customer_ids)
+        elsif method == 'activate'
+          Customer.where(id: customer_ids).update(activated: 1)
+        elsif method == 'deactivate'
+          Customer.where(id: customer_ids).update(activated: 0)
+        end
+      end
+    end
+
   end
+
+  # def destroy
+  #   puts '+++++++++++++4'
+  #   puts params
+  # end
+  #
+  # private
+  # def customer_params
+  #   params.require(:customer).permit(:name)
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
       resources :users, only: [:index, :create, :show, :update, :destroy]
 
-      resources :customers, only: [:index, :create]
+      resources :customers, only: [:index, :create, :destroy]
     end
   end
 
