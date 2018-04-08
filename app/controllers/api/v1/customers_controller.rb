d0d2b0058e9d7d617ada65596a30bc6a3da39dd2
沈斌 <bluelazysb@hotmail.com>
2018-04-08 13:58:03 +0800

    updates

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::CustomersController < Api::V1::BaseController
 
   def index
-    render json: Customer.all.to_json
+    customer = Customer.select('id as `key`, id, name, activated')
+    if params[:name]
+      customer = customer.where('name LIKE "%' + params[:name] + '%"')
+    end
+    if params[:activated]
+      customer = customer.where(activated: params[:activated])
+    end
+    list = customer.limit(10).offset(0)
+    pagination = {total: Customer.all.count, pageSize: 10, current: 1}
+    render json: {list: list, pagination: pagination}
   end
 
   def create
+    puts "=======================2"
     puts params
     render json: { status: 1 }
   end
